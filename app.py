96668cb49c7d8e3751bc00150d5c5718709be5f8
RPf357 <jampani921@gmail.com>
2023-12-01 21:35:02 -0600

    Add requirements.txt

--- app.py
+++ app.py
@@ -57,7 +57,4 @@ def predict():
         return render_template('index.html', prediction=f'Predicted Expression: {predicted_expression}', image_path=uploaded_file_path)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
-
+    app.run(debug=True)
